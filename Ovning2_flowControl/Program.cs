15125c521cfa3ac797cdc47474f024453da3ba8c
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-21 16:45:45 +0200

    Add project files.

---
+++ Ovning2_flowControl/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ovning2_flowControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
