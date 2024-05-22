88799aa61df5ef6b388af3f43d12b4ac35853765
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-05-22 16:28:34 +0200

    Create switch statement for two cases, 0 and default.

--- Ovning2_flowControl/Program.cs
+++ Ovning2_flowControl/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Ovning2_flowControl
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Ovning2_flowControl
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Välkommen till HUVUDMENY!");
+            Console.WriteLine("Du får nu flera navigerings alternativ, välj ett av alterantiven");
+            Console.WriteLine("Tryck 1 för att fortsätta");
+            Console.WriteLine("Tryck 0 för att avsluta");
+
+
+            bool isContinue = true;
+            do
+            {
+                string input = Console.ReadLine()!;
+                switch (input)
+                {
+                    case "0":
+                        isContinue = false;
+                        Console.WriteLine("Vill inte fortsätta");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Du har valt ett alternativ som inte finns, prova igen");
+                        Console.WriteLine("Tryck 1 för att fortsätta");
+                        Console.WriteLine("Tryck 0 för att avsluta");
+                        break;
+                }
+            }
+            while (isContinue);
         }
     }
 }
